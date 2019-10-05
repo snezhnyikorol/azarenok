c386a45eb0e07381247cd80a2cc97f33a942b253
Denis <den.vasilyuk@gmail.com>
2019-10-05 03:02:44 +0300

    some fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,6 +167,20 @@ function checkMap() {
   }
 }
 
+$('.questions_card').on('click', (e) => {
+    $(e.currentTarget).children('.questions_card-text').toggle('fast')
+    console.log($(e.currentTarget).find('.questions_card-img').css('transform'))
+    if ($(e.currentTarget).find('.questions_card-img').css('transform')!=='none') {
+        $(e.currentTarget).find('.questions_card-img').css('transform', 'none')
+    } else {
+        $(e.currentTarget).find('.questions_card-img').css({
+            'transform': 'rotate(180deg)',
+            'transition': 'all 400ms'
+        })
+    }
+
+})
+
 $("input[name='phone']").mask("+375 (99) 999-99-99");
 
 $('#submit').click((e) => {
